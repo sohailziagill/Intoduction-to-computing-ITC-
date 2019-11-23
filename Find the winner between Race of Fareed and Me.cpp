cfb5e18c9e56bbd121d9a3ed417091d47908d6e6
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 14:22:33 +0500

    Update and rename Race of Fareed and Me.cpp to Find the winner between Race of Fareed and Me.cpp

---
+++ Find the winner between Race of Fareed and Me.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"\n**************************************************************************";
+cout<<"\nProgrammer :  Sohail Zia Gill.";
+cout<<"\nTitle: To find the winner b/w the race and also the values are default.";
+cout<<"\nCompiler : Visual Studio 19 and Dev c++  ";
+cout<<"\nCopy Rights are Reserved";
+cout<<"\nIslamic International University Islamabad.";
+cout<<"\n***************************************************************************";
+	float as=5.9;
+	float fs=6.2;
+int time =2;
+float ad;
+float fd;
+ad=as*time;
+fd=fs*time;
+if(fd>ad)
+cout<<"Fareed won the race";
+else
+cout<<"Ahmed won the race";
+return 0;
+}
