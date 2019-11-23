dcd49711ad3a3f139b91484aaa39104e1fca1ace
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 14:20:52 +0500

    Add files via upload

---
+++ Race of Fareed and Me.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float as=5.9;
+	float fs=6.2;
+int time =2;
+float ad;
+float fd;
+ad=as*time;
+fd=fs*time;
+if(fd>ad)
+cout<<"Fareed won the race";
+else
+cout<<"Ahmed won the race";
+return 0;
+}
