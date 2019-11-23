d4c4c84b6eb775c96857e825b295f6e3f39bfeb1
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 14:14:31 +0500

    Update and rename Matric representation.cpp to Matric representation through simple array or 1D array.cpp

---
+++ Matric representation through simple array or 1D array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"\n**************************************************************************";
+cout<<"\nProgrammer :  Sohail Zia Gill.";
+cout<<"\nTitle: The lowest or minimun number in 1 D Array .";
+cout<<"\nCompiler : Visual Studio 19 and Dev c++  ";
+cout<<"\nCopy Rights are Reserved and please donot copy it";
+cout<<"\nIslamic International University Islamabad.";
+cout<<"\n***************************************************************************";
+	for(int i=1;i<=5;++i)
+	{
+		for(int j=0;j<=6;++j)
+		{
+			cout<<i*j;
+		}
+		cout<<"\n";
+	}
+	return 0;
+	
+}
