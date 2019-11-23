f7d96f342cca9d7094905b2bb7a1cb244cd90c2a
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 14:34:16 +0500

    Update and rename A4 assingment.cpp to A1 assingment sol.cpp

--- A1 assingment sol.cpp
+++ A1 assingment sol.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int main()
 {
+cout<<"\n**************************************************************************";
+cout<<"\nProgrammer :  Sohail Zia Gill.";
+cout<<"\nTitle: Array of 50 numbers if numbers are less then 25 the take sqaure of that number if greater than 25 then multiply
+with 3 and store it to the same array and also find average,sum the maximum and minimum number fron that array .";
+cout<<"\nCompiler : Visual Studio 19 and Dev c++  ";
+cout<<"\nCopy Rights are Reserved";
+cout<<"\nIslamic International University Islamabad.";
+cout<<"\n***************************************************************************";
 	float alpha[50];
 	int sum=0;
 	int h=0, j=0;
