54297a7b04e8ee3595a22431c2c9e67ae058c93d
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 14:00:10 +0500

    Add files via upload

---
+++ Average in Array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float ave;
+	int num1, num2;
+	cout<<"Enter the  1st number"<<endl;
+	cin>>num1;
+	cout<<"Enter the 2nd number";
+	cin>>num2;
+	ave=num1+num2;
+	ave=ave/2;
+	cout<<"The average of numbers are "<<ave;
+	return 0;
+	
+}
