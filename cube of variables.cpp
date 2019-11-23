2ce90c8c39483866855b83d12aac47fd44e09f4e
Sohail  Zia  Gill <57584520+sohailziagill@users.noreply.github.com>
2019-11-23 14:01:59 +0500

    Add files via upload
    It will find the cube of givven variable that is entered by the user.

---
+++ cube of variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int cube;
+	for(int i=2;i<15;++i)
+	{
+		cube=i*i*i;
+		cout<<"The cube of   "<<i<<"   is    "<<cube<<endl;
+	}
+return 0;
+}
